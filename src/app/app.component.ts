29e1202cc15fe6e287265fd522973a6bc0435098
Sacha Barragan <barragan.sacha@gmail.com>
2021-03-16 20:22:47 -0300

    This is an exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'piano';
+  colorDo: string;
+  colorRe: string;
+  colorMi: string;
+  colorFa: string;
+  colorSol: string;
+  colorLa: string;
+  colorSi: string;
+
+  constructor(){
+    this.colorDo = 'red';
+    this.colorRe = 'green';
+    this.colorMi = 'yellow'
+    this.colorFa = 'pink';
+    this.colorSol = 'blue';
+    this.colorLa = 'purple';
+    this.colorSi = 'cyan';
+  }
+
+  soundTheNote(note: number): void{
+    const audio = new Audio();
+    audio.src = `../assets/sounds/note${note}.wav`;
+    audio.load();
+    audio.play();
+    
+  }
 }
